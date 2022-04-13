8bde1f17649b35b11babc0637b5be560d6360026
Nemo1615 <inhatebi@gmail.com>
2022-04-14 00:52:52 +0300

    ребилд

---
+++ build/js/main.js
@@ -0,0 +1,5 @@
+const a = 25;
+if(a === 25){
+    console.log(25);
+}
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGEgPSAyNTtcclxuaWYoYSA9PT0gMjUpe1xyXG4gICAgY29uc29sZS5sb2coMjUpO1xyXG59Il0sImZpbGUiOiJtYWluLmpzIn0=
